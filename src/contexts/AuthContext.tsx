29c594a96a36fedc3644fa2d47800d1d09b4820e
Renan G. Castro <renangc90@gmail.com>
2022-07-18 21:22:52 -0300

    commit

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,111 @@
+import { createContext, ReactNode, useState } from 'react'
+
+import { api } from '../services/apiClient'
+
+import { destroyCookie, setCookie, parseCookies } from 'nookies'
+import Router from 'next/router'
+
+import { toast } from 'react-toastify'
+
+type AuthContextData = {
+    user: UserProps;
+    isAuthenticated: boolean;
+    signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: () => void;
+    signUp: (credentials: SignUpProps) => Promise<void>;
+}
+
+type UserProps = {
+    id: string;
+    name: string;
+    email: string;
+}
+
+type SignInProps = {
+    email: string;
+    password: string;
+}
+
+type SignUpProps = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@nextauth.token')
+        Router.push('/')
+    } catch (error) {
+        console.log('Erro ao deslogar')
+    }
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+
+    const [user, setUser] = useState<UserProps>()
+    //converter em boolean
+    const isAuthenticated = !!user
+
+    async function signIn({ email, password }: SignInProps) {
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+            })
+
+            //console.log(response.data)
+
+            const { id, name, token } = response.data
+            setCookie(undefined, '@nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, //expirar em 1 mes
+                path: "/" //quais caminhos terão acesso ao token
+            })
+
+            setUser({
+                id,
+                name,
+                email,
+            })
+
+            //passar para proximas requisições o nosso token
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            toast.success('Logado com sucesso!')
+
+            //redirecionar o user para /dashboard
+            Router.push('/dashboard')
+
+        } catch (error) {
+            toast.error('Erro ao acessar!')
+        }
+    }
+
+    async function signUp({ name, email, password }: SignUpProps) {
+        try {
+            const response = await api.post('/users', {
+                name,
+                email,
+                password
+            })
+
+            toast.success('Cadastrado com sucesso!')
+
+            Router.push('/')
+        } catch (error) {
+            toast.error('Erro ao cadastrar!')
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
